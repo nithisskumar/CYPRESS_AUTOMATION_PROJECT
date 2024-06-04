530090b59696e455caf1e428623c2327244c16fe
nithiss_010523 <nithiss@swiftant.com>
2024-06-04 14:24:20 +0530

    Recent changes

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '1wechi',
-  recordKey: '2492c151-2cc8-49e7-80fe-9bfa0376ace6',
+  record_Key: '2492c151-2cc8-49e7-80fe-9bfa0376ace6',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
