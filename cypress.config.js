6b407f4ed2ee5419b5e9a763eb78589f06f6eb9f
nithiss_010523 <nithiss@swiftant.com>
2024-06-05 12:34:43 +0530

    fourth commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '1wechi',
-  record_Key: '2492c151-2cc8-49e7-80fe-9bfa0376ace6',
+  projectId: "1wechi",
+  record_Key: "2492c151-2cc8-49e7-80fe-9bfa0376ace6",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
   },
 });
